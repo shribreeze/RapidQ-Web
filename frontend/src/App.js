00bd02c5580eac91061195861ba57023871173a4
Sameer Gautam <sg10682002@gmail.com>
2024-07-26 12:58:03 +0530

    nav done, login and signup page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,20 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Order from './components/Order';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
 import Navbar from './components/Navbar';
 import LoadingScreen from './components/LoadingScreen';
 import './App.css';
+import Cart from './components/Cart';
 
 function App() {
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        // Simulate a delay for loading screen
         const timer = setTimeout(() => {
             setLoading(false);
-        }, 2000); // Adjust time as needed
+        }, 2000);
 
         return () => clearTimeout(timer);
     }, []);
@@ -28,6 +30,9 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/order" element={<Order />} />
+                <Route path="/signIn" element={<SignIn />} />
+                <Route path="/signUp" element={<SignUp />} />
+                <Route path="/cart" element={<Cart />} />
             </Routes>
         </Router>
     );
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,12 +1,34 @@
 import React from 'react';
+import './Navbar.css';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <nav>
-            <Link to="/">Home</Link>
-            <Link to="/order">Order</Link>
+    <>
+        <nav className="navbar sticky-top navbar-expand-lg navbar-dark" id='navbar'>
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/"><img src="./LogoMini.png" style={{height:"60px"}}/></Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link className="nav-link active" aria-current="page" to="/order">Orders</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link active" aria-current="page" to="/cart"><img src="./cart.png" style={{height:"30px"}}/> Cart</Link>
+                        </li>
+                    </ul>
+                    <form className="d-flex">
+                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                        <button className="btn btn-outline-warning" type="submit">Search</button>
+                    </form>
+                    <Link className="btn btn-outline-warning me-2 m-2" to="/signIn">Sign In</Link>
+                </div>
+            </div>
         </nav>
+    </>
     );
 };
 
