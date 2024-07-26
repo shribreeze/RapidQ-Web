6f0f59295454a4cbad7e1ee7ea71c69b1994ff63
Sameer Gautam <sg10682002@gmail.com>
2024-07-26 22:43:06 +0530

    login

--- frontend/src/components/SignIn.js
+++ frontend/src/components/SignIn.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import './SignIn.css';
 
 
 
@@ -17,11 +18,11 @@ const SignIn = () => {
 
   return (
     <>
-        <section className="bg-light p-3 p-md-4 p-xl-5">
+      <section className="bg-light p-3 p-md-4 p-xl-5">
       <div className="container">
         <div className="row justify-content-center">
           <div className="col-12 col-md-9 col-lg-7 col-xl-6 col-xxl-5">
-            <div className="card border border-light-subtle rounded-4">
+            <div className="card border border-light-subtle rounded-4" id='SignInCard'>
               <div className="card-body p-3 p-md-4 p-xl-5">
                 <div className="row">
                   <div className="col-12">
@@ -79,7 +80,7 @@ const SignIn = () => {
                     </div>
                     <div className="col-12">
                       <div className="d-grid">
-                        <button className="btn bsb-btn-xl btn-primary" type="submit">Log in now</button>
+                        <button className="btn bsb-btn-xl" id='SignInBtn' type="submit">Log in now</button>
                       </div>
                     </div>
                   </div>
