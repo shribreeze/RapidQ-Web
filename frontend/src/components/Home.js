b7cca59d98c5bbef8d80780469af735bd66adf1f
Sameer Gautam <sg10682002@gmail.com>
2024-07-27 14:13:25 +0530

    minor update

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h1>Welcome to QuickQueue</h1>
+            <h1>Quick Orders, Quality Meals</h1>
             <p>Order your food quickly and efficiently.</p>
         </div>
     );
