2e50a1ddff6bfac8ec7c33244c678336e79466bb
Sameer Gautam <sg10682002@gmail.com>
2024-07-28 22:04:52 +0530

    auth bg

--- frontend/src/components/SignIn.js
+++ frontend/src/components/SignIn.js
@@ -44,7 +44,7 @@ const SignIn = () => {
     <>
       <section className="p-3 p-md-4 p-xl-5">
         <div className="container">
-          <div className="row justify-content-center">
+          <div className="row justify-content-left">
             <div className="col-12 col-md-9 col-lg-7 col-xl-6 col-xxl-5">
               <div className="card border border-light-subtle rounded-4" id='SignInCard'>
                 <div className="card-body p-3 p-md-4 p-xl-5">
