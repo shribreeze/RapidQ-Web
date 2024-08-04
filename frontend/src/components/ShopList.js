3ddb39bb2c329ff0c3eb33d924d76c6149065159
Sameer Gautam <sg10682002@gmail.com>
2024-08-04 18:08:38 +0530

    popular dishes added

--- frontend/src/components/ShopList.js
+++ frontend/src/components/ShopList.js
@@ -1,28 +1,74 @@
-import React from 'react'
-import CardComponent from './CardComponent'
+import React from 'react';
+import CardComponent from './CardComponent';
 
 const ShopList = () => {
   return (
     <>
-      <div className="mx-4 mb-4 mt-5">
-          <p className='display-5'>Food Outlets for you:</p>
+      <div className="m-4">
+        <p className='display-5'>Food Outlets for you:</p>
       </div>
-
       <div className="container">
         <div className="row">
-          {[...Array(8)].map((_, index) => (
-            <div className="col-12 col-sm-6 col-md-4 col-lg-3" key={index}>
-              <CardComponent
-                imgSrc={`https://via.placeholder.com/150?text=Image+${index + 1}`}
-                altText={`Example Image ${index + 1}`}
-                cardText={`Quick example text for card ${index + 1} to build on the card title and make up the bulk of the card's content.`}
-                />
-            </div>
-          ))} 
+          <div className="col-12 col-md-6 col-lg-3">
+            <CardComponent
+              imgSrc="/ShopPics/fast-food-cafe.jpeg"
+              altText="Example Image 1"
+              cardText="Some quick example text to build on the card title and make up the bulk of the card's content."
+            />
+          </div>
+          <div className="col-12 col-md-6 col-lg-3">
+            <CardComponent
+              imgSrc="/ShopPics/singhs-baker.jpeg"
+              altText="Example Image 2"
+              cardText="Another quick example text to build on the card title and make up the bulk of the card's content."
+            />
+          </div>
+          <div className="col-12 col-md-6 col-lg-3">
+            <CardComponent
+              imgSrc="https://via.placeholder.com/150"
+              altText="Example Image 3"
+              cardText="Yet another quick example text to build on the card title and make up the bulk of the card's content."
+            />
+          </div>
+          <div className="col-12 col-md-6 col-lg-3">
+            <CardComponent
+              imgSrc="https://via.placeholder.com/150"
+              altText="Example Image 4"
+              cardText="A different quick example text to build on the card title and make up the bulk of the card's content."
+            />
+          </div>
+          <div className="col-12 col-md-6 col-lg-3">
+            <CardComponent
+              imgSrc="https://via.placeholder.com/150"
+              altText="Example Image 5"
+              cardText="Some more quick example text to build on the card title and make up the bulk of the card's content."
+            />
+          </div>
+          <div className="col-12 col-md-6 col-lg-3">
+            <CardComponent
+              imgSrc="https://via.placeholder.com/150"
+              altText="Example Image 6"
+              cardText="Even more quick example text to build on the card title and make up the bulk of the card's content."
+            />
+          </div>
+          <div className="col-12 col-md-6 col-lg-3">
+            <CardComponent
+              imgSrc="https://via.placeholder.com/150"
+              altText="Example Image 7"
+              cardText="Another one quick example text to build on the card title and make up the bulk of the card's content."
+            />
+          </div>
+          <div className="col-12 col-md-6 col-lg-3">
+            <CardComponent
+              imgSrc="https://via.placeholder.com/150"
+              altText="Example Image 8"
+              cardText="The last quick example text to build on the card title and make up the bulk of the card's content."
+            />
+          </div>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default ShopList
+export default ShopList;
