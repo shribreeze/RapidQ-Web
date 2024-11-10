ada8e7c852896c8a615e048ed3711b50b27ce0b7
Divyanshu2309 <divyanshuagarwal94154@gmail.com>
2024-11-10 16:35:10 +0530

    otp to string

--- frontend/src/components/Order.js
+++ frontend/src/components/Order.js
@@ -149,8 +149,9 @@ const Orders = () => {
     };
 
     const generateOTP = () => {
-        return Math.floor(1000 + Math.random() * 9000); // Generates a random 4-digit OTP
+        return (Math.floor(1000 + Math.random() * 9000)).toString(); // Generates a random 4-digit OTP and converts it to a string
     };
+
 
     const handlePaymentConfirmation = async (confirmation) => {
         if (confirmation === 'yes' && selectedOrderId) {
